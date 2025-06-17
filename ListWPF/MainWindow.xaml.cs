579dca908995e25163439ec4ce816b1ff6cc7f60
Huynh An Khuong <huynhankhuong0511@gmail.com>
2025-06-17 11:24:36 +0700

    List WPF

---
+++ ListWPF/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Windows;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ListWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<int> dsDuLieu=new List<int>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void HienThiDanhSach()
+        {
+            lstDuLieu.Items.Clear();
+            for (int i = 0; i < dsDuLieu.Count; i++)
+            {
+                int x = dsDuLieu[i];
+                lstDuLieu.Items.Add(x);
+            }
+        }
+
+        private void BtnThem_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(txtGiaTri.Text);
+            dsDuLieu.Add(x);
+            HienThiDanhSach();
+            txtGiaTri.Text = "";
+        }
+        private void BtnChen_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(txtGiaTriChen.Text);
+            int vt = int.Parse(txtViTriChen.Text);
+            dsDuLieu.Insert(vt,x);
+            HienThiDanhSach();
+            txtViTriChen.Text = "";
+            txtGiaTriChen.Text = "";
+        }
+        private void BtnSapXepTang_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            HienThiDanhSach();
+        }
+        private void BtnSapXepGiam_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            dsDuLieu.Reverse();
+            HienThiDanhSach();
+        }
+        private void BtnXoa1PhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử","Thông báo lỗi",MessageBoxButton.OK);
+                return;
+            }
+            dsDuLieu.RemoveAt(lstDuLieu.SelectedIndex);
+            HienThiDanhSach();
+        }
+
+        private void BtnXoaNhieuPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử", "Thông báo lỗi", MessageBoxButton.OK);
+                return;
+            }
+            while (lstDuLieu.SelectedItems.Count > 0)
+            {
+                int data = (int)lstDuLieu.SelectedItems[0];
+                dsDuLieu.Remove(data);
+                lstDuLieu.Items.Remove(data);
+            }
+            HienThiDanhSach();
+        }
+
+        private void BtnXoaToanBoPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Clear();
+            HienThiDanhSach();
+        }
+    }
+}
